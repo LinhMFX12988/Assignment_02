00f5f592bfed50b2bedba8e62dfa25f854ddc683
linhnmfx12988 <linhnmfx12988@funix.edu.vn>
2022-01-04 16:51:06 +0700

    add staff

--- src/App.js
+++ src/App.js
@@ -7,13 +7,25 @@ import Salary from "./components/Salary";
 import Footer from "./components/Footer";
 import "./App.css";
 import { STAFFS, DEPARTMENTS } from "./shared/staffs";
-import { BrowserRouter } from "react-router-dom";
-import { Switch, Route, Redirect } from 'react-router-dom';
+import {BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 function App() {
-  const [staff] = useState(STAFFS);
+  const [staff, setStaff] = useState(STAFFS);
   const [department] = useState(DEPARTMENTS);
 
+  //-------------------Add Staff---------------------
+  const addStaff = (newStaff) => {
+    let newStaffs = staff.concat([{
+      ...newStaff,
+      id: staff.length + 1,
+      image: '/assets/images/alberto.jpg',
+      department: department.filter(x => x.id === newStaff.department)[0]
+    }]);
+
+    setStaff(newStaffs);
+  }
+
+  //--------------Render detail staff----------------
   const renderDetailStaff = ({ match }) =>
       <StaffDetail
         staffs={staff.filter((staffs) =>
@@ -25,7 +37,7 @@ function App() {
       <div>
         <Header />
         <Switch>
-          <Route exact path="/staffs" component={() => <StaffList staffs={staff} />} />
+          <Route exact path="/staffs" component={() => <StaffList staffs={staff} addStaff={addStaff} />} />
           <Route path="/staffs/:id" component={renderDetailStaff} />
           <Route path="/department" component={() => <Department departments={department} />} />
           <Route path="/salary" component={() => <Salary salarys={staff} />} />
